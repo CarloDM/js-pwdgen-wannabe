80fe69697cdbf86ecca9b7fc0cc1c07fa88ede19
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-17 15:03:46 +0100

    [js] output generated

--- js/scypt.js
+++ js/scypt.js
@@ -4,9 +4,20 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito23
 
-
+1. variabile chiedere nome utente
+2. variabile chiedere cognome
+3. variabile chiedere colore preferito
+4. scrivi su html nome cognome colorPreferito
 
 */
 
+const nome = prompt('qual è il tuo nome?')
 
+const cognome = prompt('qual è il tuo cognome?')
+
+const colore = prompt('qual è il tuo colore preferito?')
+
+let pssw = nome + cognome  + colore 
+
+console.log(pssw)
 
